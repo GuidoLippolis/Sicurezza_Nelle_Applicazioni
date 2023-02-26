64281cc53d8146b62663b9c9eb87d888d289758a
Guido Lippolis <guidolippolis2012@libero.it>
2023-02-26 16:42:39 +0100

    Modified signature of method: generateHash()

--- WebApp/src/main/java/passwordUtils/PasswordUtils.java
+++ WebApp/src/main/java/passwordUtils/PasswordUtils.java
@@ -1,31 +1,32 @@
 package passwordUtils;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class PasswordUtils {
 
-	public static String generateHash(String data, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
+//	public static String generateHash(String data, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
+	public static byte[] generateHash(String data, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
 		
 		MessageDigest digest = MessageDigest.getInstance(algorithm);
 		digest.reset();
 		digest.update(salt);
 		byte[] hash = digest.digest(data.getBytes());
-		return bytesToStringHex(hash);
+		return hash;
+//		return bytesToStringHex(hash);
 		
 	}
 	
-	private static String bytesToStringHex(byte[] bytes) {
-		
-		return new BigInteger(1, bytes).toString(16);
-		
-	}
+//	private static String bytesToStringHex(byte[] bytes) {
+//		
+//		return new BigInteger(1, bytes).toString(16);
+//		
+//	}
 	
-	public static byte[] createSalt() {
+	public static byte[] createSalt(int length) {
 		
-		byte[] bytes = new byte[20];
+		byte[] bytes = new byte[length];
 		SecureRandom random = new SecureRandom();
 		random.nextBytes(bytes);
 		return bytes;
