d67c5c5c92d4e63c080168b9790f824de6753e7e
Guido Lippolis <guidolippolis2012@libero.it>
2023-02-28 18:32:04 +0100

    Updated class PasswordUtils

--- WebApp/src/main/java/passwordUtils/PasswordUtils.java
+++ WebApp/src/main/java/passwordUtils/PasswordUtils.java
@@ -6,22 +6,44 @@ import java.security.SecureRandom;
 
 public class PasswordUtils {
 
-	public static byte[] generateHash(String data, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
+	public static byte[] generateHash(byte[] password, String algorithm) throws NoSuchAlgorithmException {
 		
-		MessageDigest digest = MessageDigest.getInstance(algorithm);
-		digest.reset();
-		digest.update(salt);
-		byte[] hash = digest.digest(data.getBytes());
-		return hash;
+		byte[] salt = createSalt(10);
+		
+		byte[] input = appendByteArrays(password, salt);
+		
+		MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+		
+		byte[] hashValue = messageDigest.digest(input);
+		
+		clearArray(password);
+		clearArray(input);
+		clearArray(salt);
+
+		return hashValue;
 		
 	}
 	
-	public static byte[] createSalt(int length) {
+	private static byte[] appendByteArrays(byte[] a, byte[] b) {
+	    byte[] result = new byte[a.length + b.length];
+	    System.arraycopy(a, 0, result, 0, a.length);
+	    System.arraycopy(b, 0, result, a.length, b.length);
+	    return result;
+	}
+	
+	private static byte[] createSalt(int length) {
 		
 		byte[] bytes = new byte[length];
 		SecureRandom random = new SecureRandom();
 		random.nextBytes(bytes);
 		return bytes;
+		
+	}
+	
+	private static void clearArray(byte[] array) {
+		
+		for(int i=0; i<array.length; i++)
+			array[i] = 0;
 		
 	}
 	
