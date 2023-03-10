c252a29fd66dc4f7370d1f6f38794a7304d781ef
Guido Lippolis <guidolippolis2012@libero.it>
2023-03-10 22:08:56 +0100

    Fixed signUp() method

--- WebApp/src/main/java/dao/UserDAO.java
+++ WebApp/src/main/java/dao/UserDAO.java
@@ -21,7 +21,8 @@ public class UserDAO {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection connection = null;
+		Connection usersConnection = null;
+		Connection passwordsConnection = null;
 		
 		PreparedStatement usersStatement = null;
 		PreparedStatement passwordsStatement = null;
@@ -41,12 +42,14 @@ public class UserDAO {
 		byte[] hashedPassword = PasswordUtils.generateHash(password, salt, "SHA-256");
 		
 		try {
+
+			usersConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_db", "root", "WgAb_9114_2359");
+			passwordsConnection= DriverManager.getConnection("jdbc:mysql://localhost:3306/passwords_db", "root", "WgAb_9114_2359");
 			
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_db", "root", "WgAb_9114_2359");
+			usersConnection.setAutoCommit(false);
+			passwordsConnection.setAutoCommit(false);
 			
-			connection.setAutoCommit(false);
-			
-			usersStatement = connection.prepareStatement("INSERT INTO users(email, photo) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+			usersStatement = usersConnection.prepareStatement("INSERT INTO users(email, photo) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
 			
 			usersStatement.setString(1, user.getEmail());
 			usersStatement.setBytes(2, user.getPhoto());
@@ -60,40 +63,33 @@ public class UserDAO {
 				
 				userId = resultSetUsers.getInt(1);
 				
+				usersConnection.commit();
+				
 			}
 			
-			passwordsStatement = connection.prepareStatement("INSERT INTO passwords_db.passwords(user_id, password) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+			passwordsStatement = passwordsConnection.prepareStatement("INSERT INTO passwords_db.passwords(user_id, password) VALUES(?,?)");
 
 			passwordsStatement.setInt(1, userId);
 			passwordsStatement.setBytes(2, hashedPassword);
 			
-			int rowsAffectedPasswords = passwordsStatement.executeUpdate();
+			passwordsStatement.executeUpdate();
 			
-			if(rowsAffectedUsers == 1 && rowsAffectedPasswords == 1) {
-				
-				connection.commit();
-				return true;
-				
-			} else {
-				
-				connection.rollback();
-				return false;
-				
-			}
+			passwordsConnection.commit();
+			
+			return true;
 			
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			
-			if(connection != null)
-				connection.rollback();
-			
 			return false;
 		
 		} finally {
 			
-			if(connection != null)
-				connection.close();
+			if(usersConnection != null)
+				usersConnection.close();
+			
+			if(usersConnection != null)
+				usersConnection.close();
 			
 			if(usersStatement != null)
 				usersStatement.close();
@@ -106,6 +102,98 @@ public class UserDAO {
 			
 		}
 		
+		
 	}
+	
+//	public static boolean signUp(final User user, byte[] password) throws SQLException, ClassNotFoundException, NoSuchAlgorithmException {
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		Connection connection = null;
+//		
+//		PreparedStatement usersStatement = null;
+//		PreparedStatement passwordsStatement = null;
+//		
+//		ResultSet resultSetUsers = null;
+//		
+//		int userId = 0;
+//		
+//		/*
+//		 * TODO: Il parametro di createSalt va recuperato da un file .properties
+//		 * TODO: Le query e l'algoritmo di hashing vanno anche recuperate da un file .properties
+//		 * TODO: Aggiungere query INSERT sul database salts_db
+//		 * 
+//		 * */
+//		
+//		byte[] salt = PasswordUtils.createSalt(10);
+//		byte[] hashedPassword = PasswordUtils.generateHash(password, salt, "SHA-256");
+//		
+//		try {
+//			
+//			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_db", "root", "WgAb_9114_2359");
+//			
+//			connection.setAutoCommit(false);
+//			
+//			usersStatement = connection.prepareStatement("INSERT INTO users(email, photo) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+//			
+//			usersStatement.setString(1, user.getEmail());
+//			usersStatement.setBytes(2, user.getPhoto());
+//			
+//			int rowsAffectedUsers = usersStatement.executeUpdate();
+//			
+//			if(rowsAffectedUsers > 0) {
+//				
+//				resultSetUsers = usersStatement.getGeneratedKeys();
+//				resultSetUsers.next();
+//				
+//				userId = resultSetUsers.getInt(1);
+//				
+//			}
+//			
+//			passwordsStatement = connection.prepareStatement("INSERT INTO passwords_db.passwords(user_id, password) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+//
+//			passwordsStatement.setInt(1, userId);
+//			passwordsStatement.setBytes(2, hashedPassword);
+//			
+//			int rowsAffectedPasswords = passwordsStatement.executeUpdate();
+//			
+//			if(rowsAffectedUsers == 1 && rowsAffectedPasswords == 1) {
+//				
+//				connection.commit();
+//				return true;
+//				
+//			} else {
+//				
+//				connection.rollback();
+//				return false;
+//				
+//			}
+//			
+//		} catch (Exception e) {
+//
+//			e.printStackTrace();
+//			
+//			if(connection != null)
+//				connection.rollback();
+//			
+//			return false;
+//		
+//		} finally {
+//			
+//			if(connection != null)
+//				connection.close();
+//			
+//			if(usersStatement != null)
+//				usersStatement.close();
+//			
+//			if(passwordsStatement != null)
+//				passwordsStatement.close();
+//			
+//			if(resultSetUsers != null)
+//				resultSetUsers.close();
+//			
+//		}
+//		
+//	}
 	
 }
