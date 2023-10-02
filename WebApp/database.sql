5d96612c0d1c8e60f800c43a870e85d61dffa745
GuidoLippolis <guidolippolis2012@libero.it>
2023-10-02 20:29:12 +0200

    - Updated passwords in application.properties - Commit script database.sql

---
+++ WebApp/database.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS passwords_db;
+DROP DATABASE IF EXISTS salts_db;
+DROP DATABASE IF EXISTS users_db;
+
+CREATE DATABASE IF NOT EXISTS users_db;
+CREATE DATABASE IF NOT EXISTS passwords_db;
+CREATE DATABASE IF NOT EXISTS salts_db;
+
+USE users_db;
+
+CREATE TABLE users (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  photo LONGBLOB
+);
+
+USE passwords_db;
+
+CREATE TABLE passwords (
+
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    password LONGBLOB NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users_db.users(id)
+
+);
+
+USE salts_db;
+
+CREATE TABLE IF NOT EXISTS salts (
+
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    salt LONGBLOB NOT NULL,
+    FOREIGN KEY (username) REFERENCES users_db.users(username)
+
+);
